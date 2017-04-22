50cd142be39defac56887edab777b8e5fad0d30a
Duarte Carvalho <up201503661@fe.up.pt>
2017-04-22 15:12:01 +0100

    README finished and script added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Fresh-Ubunto-Installs
-Script that installs Grub-Costumizer, Atom, Sublime-Text3, Maxima, Java8, Clang, Spotify and Git
+Script that installs Grub-Costumizer, Atom, Sublime-Text3, Maxima, Java8, Clang, Spotify and GitKraken
+
+just open terminal:
+
+$ chmod 777 installs.sh
+
+$ ./installs.sh
---
+++ installs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Repos addition
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y		#GRUB_COSTUMIZER
+sudo add-apt-repository ppa:webupd8team/atom -y					#ATOM
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y			#SUBLIME
+sudo add-apt-repository ppa:webupd8team/java -y					#JAVA
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"	#SPOTIFY
+#Repos addition
+
+sudo apt update -y
+
+sudo apt install atom -y							#ATOM
+sudo apt install grub-customizer -y						#GRUB_COSTUMIZER
+sudo apt install clang -y							#CLANG
+sudo apt install sublime-text-installer -y					#SUBLIME
+sudo apt install maxima -y							#MAXIMA
+sudo apt install oracle-java8-installer -y					#JAVA
+
+#SPOTIFY
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886 -y	
+sudo apt-get update -qq -y							
+sudo apt-get install spotify-client -y						
+#SPOTIFY
+
+#GITKRAKEN
+wget https://release.gitkraken.com/linux/gitkraken-amd64.tar.gz			
+tar -xvzf gitkraken-amd64.tar.gz						
+cd ./gitkraken
+chmod 777 gitkraken
+./gitkraken
+#GITKRAKEN
+
+
